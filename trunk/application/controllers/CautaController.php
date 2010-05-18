e96d1b89516504deccca070a41ffb857534cfb08
raresvla <raresvla@739866c1-f3ea-d760-817f-5cf63da7293b>
2010-05-18 21:15:43 +0000

    Work on building Solr index

--- trunk/application/controllers/CautaController.php
+++ trunk/application/controllers/CautaController.php
@@ -11,6 +11,24 @@ class CautaController extends Zend_Controller_Action
      */
     public function indexAction()
     {
+        $produs = Doctrine::getTable('Produse')->find(1);
+        $data = array(
+            'id' => $produs->id,
+            'producator' => $produs->marca,
+            'codProdus' => $produs->cod_produs,
+            'denumire' => $produs->denumire,
+            'categorie' => $produs->Categorii->denumire,
+            'caracteristici' => array('rosu', 'negru'),
+            'greutate' => $produs->greutate,
+            'pret' => $produs->pret,
+            'popularitate' => '11',
+            'inStoc' => true
+        );
+
         $solr = new MyShop_Solr();
+        //$solr->deleteByQuery('note*');
+        $solr->index(1);
+        $solr->index(2);
+        $solr->index(3);
     }
 }
