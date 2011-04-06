c7db5e934b8c49e5baa978afd25585396a5b881c
raresvla@gmail.com <raresvla@gmail.com@739866c1-f3ea-d760-817f-5cf63da7293b>
2011-04-06 18:55:34 +0000

    Changes to config and Solr library

--- trunk/admin/config/server.php
+++ trunk/admin/config/server.php
@@ -1,4 +1,4 @@
 <?php
-$CALE_FIZICA_SERVER   = 'c:\\www\\myshop\\admin\\';     // cu slash final si \\ in loc de \ pe windows
+$CALE_FIZICA_SERVER   = 'c:\\www\\html\\myshop\\admin\\';     // cu slash final si \\ in loc de \ pe windows
 $CALE_VIRTUALA_SERVER = 'http://admin.myshop.ro/';           // cu slash final
 ?>
